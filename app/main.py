6c8aa61bf716650b78184698f462b76441cef951
Ian Green <igreenop1@gmail.com>
2025-05-22 09:26:11 -0700

    add update and retrieve all api calls

--- app/main.py
+++ app/main.py
@@ -1,25 +1,53 @@
-from fastapi import FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, HTTPException, Depends
+from database import get_db
+from models import User
 
 app = FastAPI()
 
-users = {}
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello World :D"}
-
 @app.post("/create_user")
-def create_user(username: str, password: str):
-    if username in users:
+def create_user(username: str, password: str, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.username == username).first()
+    if existing_user:
         raise HTTPException(status_code=400, detail="Username already exists")
-    users[username] = password
+
+    new_user = User(username=username, password=password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
     return {"message": "User created successfully"}
 
-@app.delete("/remove_user")
-def remove_user(username: str, password: str):
-    if username not in users:
-        raise HTTPException(status_code=400, detail="User does not exist")
-    if password != users[username]:
+@app.delete("/delete_user")
+def delete_user(username:str, password: str, db : Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.username == username).first()
+    if not existing_user:
+        raise HTTPException(status_code=400, detail="Username does not exist")
+    if existing_user.password != password:
         raise HTTPException(status_code=400, detail="Incorrect password")
-    result = users.pop(username)
+
+    db.delete(existing_user)
+    db.commit()
+
     return {"message": "User deleted successfully"}
+
+@app.get("/list_users")
+def list_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
+
+@app.put("/update_password")
+def update_password(username: str, old_password: str, new_password: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == username).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user.password != old_password:
+        raise HTTPException(status_code=400, detail="Incorrect password")
+
+    user.password = new_password
+    db.commit()
+    db.refresh(user)
+
+    return {"message": "Password updated successfully"}
+
